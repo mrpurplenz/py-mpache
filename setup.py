a2d803028161f0ee19c885fc5adc322fd6bb6a30
mrpurple <mrpurplenz@gmail.com>
2016-09-15 15:44:40 +1200

    Initial commit

---
+++ setup.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python
+
+"""
+This file is part of py-mpache.
+
+py-mpache is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+py-mpache is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with py-sonic.  If not, see <http://www.gnu.org/licenses/>
+"""
+
+from distutils.core import setup
+from libmpache import __version__ as version
+
+setup(name='py-mpache',
+    version=version,
+    author='mr purple',
+    author_email='mrpurplenz@gmail.com',
+    url='https://github.com/mrpurplenz/py-mpache',
+    download_url - 'https://github.com/mrpurplenz/py-mpache/tarball/0.1',
+    description='A python wrapper library for the Ampache XML API.  '
+        'http://ampache.org',
+    long_description='This is a basic wrapper library for the Ampache '
+        'XML API. This will allow you to connect to your server and retrieve '
+        'information and have it returned in basic Python types.',
+    packages=['libmpache'],
+    package_dir={'libmpache': 'libmpache'},
+    classifiers=[
+        'Development Status :: 1 - Alpha',
+        'Intended Audience :: System Administrators',
+        'Intended Audience :: Information Technology',
+        'License :: OSI Approved :: GNU General Public License (GPL)',
+        'Natural Language :: English',
+        'Operating System :: POSIX',
+        'Programming Language :: Python',
+        'Topic :: System :: Systems Administration',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Topic :: Software Development :: Libraries',
+        'Topic :: System',
+    ]
+)
